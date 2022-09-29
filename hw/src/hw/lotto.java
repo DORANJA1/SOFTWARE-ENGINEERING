4a606906649e1d216291aae3b43863f5cb78c299
44395515_seunghoon_kwak <60720089+DORANJA1@users.noreply.github.com>
2022-09-30 07:08:21 +0900

    hw

---
+++ hw/src/hw/lotto.java
@@ -0,0 +1,55 @@
+package hw;
+
+public class lotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+				int [] lotto = new int[6];
+				getLotto(lotto);
+				
+				
+			}
+			
+			public static void getLotto(int [] lotto) {
+				
+				int [] sub = new int[6];
+				for(int i=0; i<6;i++) {
+					boolean dp=false;
+					lotto[i]=setRandom();
+					
+					int j=0;
+					while(j<i) {
+						int n=(int)(Math.random()*45)+1;
+						if(lotto[i]==lotto[j]) {
+							lotto[i]=n;
+						}else {
+							j++;
+						}
+						
+						
+						
+						
+					}
+				}
+				
+				
+				
+				
+				
+				for(int i=0; i<6; i++) {
+					System.out.println("번호는"+lotto[i]);
+				}
+				
+			}
+			
+			public static int setRandom() {
+			
+				return (int)(Math.random()*45)+1;
+			}
+
+		}
+
+	
+
+
